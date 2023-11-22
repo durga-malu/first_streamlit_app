e395c5c5005eb7af24c21c56fa7c0ac349ce103b
durga-malu <151632537+durga-malu@users.noreply.github.com>
2023-11-22 15:43:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 
+import streamlit
+import requests
+import pandas as pd  # Corrected the import statement for pandas
+from urllib.error import URLError  # Corrected the import statement for URLError
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -47,12 +52,16 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        
+        if fruityvice_response.status_code == 200:
+            fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+            streamlit.dataframe(fruityvice_normalized)
+        else:
+            streamlit.error(f"Fruit not found: {fruit_choice}")
 
 except URLError as e:
-streamlit.error()
+    streamlit.error(f"Error accessing the API: {e}")
 
 
 
