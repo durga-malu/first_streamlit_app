176c0d265cf30b3c4236133da1ade14e03f6bcce
durga-malu <151632537+durga-malu@users.noreply.github.com>
2023-11-22 17:14:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,3 +86,9 @@ def insert_row_snowflake(new_fruit):
     except Exception as e:
         return f"Error adding {new_fruit}: {e}"
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+back_from_function = insert_row_snowflake(add_my_fruit)
+streamlit.text(back_from_function )
+
